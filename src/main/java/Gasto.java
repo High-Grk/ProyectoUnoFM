3682dccb33d354954faa261088cb61ebd2806ac6
Fernando Martinez 1 <fmartinez@uno.com>
2020-12-20 21:39:43 -0300

    implementando Gastos

--- src/main/java/Gasto.java
+++ src/main/java/Gasto.java
@@ -1,2 +1,15 @@
 public class Gasto {
+    String gasto;
+    int valor;
+    boolean pagado;
+
+    public Gasto(String gasto, int valor, boolean pagado) {
+        this.gasto = gasto;
+        this.valor = valor;
+        this.pagado = pagado;
+    }
+
+    public int getValor() {
+        return valor;
+    }
 }
